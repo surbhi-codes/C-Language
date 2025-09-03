aad88988a9ffa7af523df0ddd05f2c0bc10034fe
surbhi-codes <jhasurbhi21@gmail.com>
2025-09-03 21:24:34 +0530

    add variables.c

---
+++ variables.c
@@ -0,0 +1,25 @@
+#include <studio.h>
+
+int main() {
+    int age= 22;
+    char star= '*';
+    float pi= 3.14;
+
+    a = 30;                   /*a) It's case sensitive*/
+    A = 50;
+
+    int number= 25;           /* b) 1st character is alphabet or '_' */
+
+     int final_price= 200;    /* c) no comma/blank space
+                               d) No other symbol other than '_'*/
+    return 0;
+}
+
+/*VARIABLES- Variable is the name of a memory location which stores some data.*/
+
+/*Constants- Values that don't change(fixed)
+Types:-
+1.Integer = 1,2,0,-1,-2
+2. Real   = 1.0, 3.0, 3.14
+3. Character = 'a', 'A', '#', '&' 
+*/
