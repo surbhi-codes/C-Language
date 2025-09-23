33fa4a0e451f022baf06d585564c3bc82977553c
surbhi-codes <jhasurbhi21@gmail.com>
2025-09-23 08:22:42 +0530

    Add program to calculate area of a triangle using three sides

---
+++ 11_Triangle.c
@@ -0,0 +1,21 @@
+/*WAP in C to find The area of a triangle when three sides are given.*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c, s, area;
+    printf("Enter the three sides of the triangle: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+
+    // Semi-perimeter
+    s = (a + b + c)/2;
+
+    // Area using Heron's formula
+    area = sqrt (s * (s-a) * (s-b) * (s-c));
+
+    printf ("Area of triangle is: %2.f\n", area);
+    return 0;
+}
