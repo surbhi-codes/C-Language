40a874ccd4e15560e966c210bd68f00261507253
surbhi-codes <jhasurbhi21@gmail.com>
2025-09-23 19:15:17 +0530

    add star pattern program

---
+++ 1. Basic ques/14_Star.c
@@ -0,0 +1,28 @@
+/*Star Pattern*/
+#include <stdio.h>
+int main()
+{   
+    printf("Right Half Pyramid\n");
+    printf("*\n");
+    printf("* *\n");
+    printf("* * *\n");
+    printf("* * * *\n");
+    printf("* * * * *\n");
+
+
+    printf("Reverse Right Half Pyramid\n");
+    printf("* * * * *\n");
+    printf("* * * *\n");
+    printf("* * *\n");
+    printf("* *\n");
+    printf("*\n");
+
+
+    printf("Left Half Pyramid\n");
+    printf("       *\n");
+    printf("     * *\n");
+    printf("   * * *\n");
+    printf("  * * * *\n");
+    printf("* * * * *\n");
+
+}
---
+++ 1. Basic ques/15_2Star.c
@@ -0,0 +1,8 @@
+/*Show pattern using single print statement */
+
+#include <stdio.h>
+int main()
+{
+   printf("Right Half Pyramid\n*\n* *\n* * *\n* * * *\n* * * * *\n");
+   
+}
