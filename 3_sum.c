4bfe589dfb037beef337a8cdd38c2e8892679f9b
surbhi-codes <jhasurbhi21@gmail.com>
2025-09-23 06:56:05 +0530

    Add program to calculate sum of two numbers

--- 3_sum.c
+++ 3_sum.c
@@ -5,7 +5,7 @@ int num1, num2, sum;
 
 printf("Enter first number: ");
 scanf("%d", &num1);
-printf("Enter secpnd number: ");
+printf("Enter second number: ");
 scanf("%d", &num2);
 
 sum= num1 + num2;
