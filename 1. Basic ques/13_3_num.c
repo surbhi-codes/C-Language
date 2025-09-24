9fb8a22dcbae6b29bee113f8d19f5fdc0839b9a6
surbhi-codes <jhasurbhi21@gmail.com>
2025-09-24 20:58:40 +0530

    add program to find largest value

--- 1. Basic ques/13_3_num.c
+++ 1. Basic ques/13_3_num.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 int main()
 {
-    int a, b, c, largest;
+    int a, b, c, large;
     printf("Enter three numbers: ");
     scanf("%d %d %d", &a, &b, &c);
 
     // Using nested ternary operator
-    largest = (a > b) ? ((a > c) ? a : c)
+    large = (a > b) ? ((a > c) ? a : c)
                       : ((b > c) ? b : c);
 
-   printf("The largest number is: %d\n", largest);
+   printf("The largest number is: %d\n", large);
 }
