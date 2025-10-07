5376da27752de64d2007597b7546832e9439147c
surbhi-codes <jhasurbhi21@gmail.com>
2025-10-07 17:18:37 +0530

    Add 2 star pattern in C

--- 1. Basic ques/15_2Star.c
+++ 1. Basic ques/15_2Star.c
@@ -4,5 +4,6 @@
 int main()
 {
    printf("Right Half Pyramid\n*\n* *\n* * *\n* * * *\n* * * * *\n");
-   
+   printf("Reverse Right Half Pyramid\n* * * * *\n* * * *\n* * *\n* *\n*\n");
+   printf("Left Half Pyramid\n      *\n    * *\n  * * *\n  * * * *\n* * * * *\n");  
 }
