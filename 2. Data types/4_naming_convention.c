403cde47408d3da8aa0a56f238f76b18231473b7
surbhi-codes <jhasurbhi21@gmail.com>
2025-10-08 08:10:00 +0530

    Add a program  of name conventions

---
+++ 2. Data types/4_naming_convention.c
@@ -0,0 +1,13 @@
+/* Define variables for storing a user's first name, last name, and age 
+using appropriate naming conventions and then display them.
+ */
+
+ #include <stdio.h>
+ int main() {
+   char first_name [20]= "Ram";
+   char last_name [20] = "India";
+   int age = 45;
+
+   printf("First_name: %s,last_name: %s, Age: %d",first_name, last_name,age);
+   return 0;
+   }
